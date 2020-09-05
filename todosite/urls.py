05e4c6e42b533aeb3bd3b29d7775ea83724a2533
udit5656 <uditgarg5656@gmail.com>
2020-09-05 16:41:46 +0530

    Minor changes in urls routing

--- todosite/urls.py
+++ todosite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='accounts/login/')),
     path('admin/', admin.site.urls),
     path('todoapp/', include('todoapp.urls')),
     path('accounts/', include('accounts.urls')),
